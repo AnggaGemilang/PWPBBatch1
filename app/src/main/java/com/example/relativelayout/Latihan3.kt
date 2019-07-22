a0a3318d0ea93f71565f3a62cdd89a2e18e7804b
Angga.Gemilang <angga.gemilang.mu@gmail.com>
2019-07-22 23:28:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/relativelayout/Latihan3.kt
@@ -0,0 +1,12 @@
+package com.example.relativelayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Latihan3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latihan3)
+    }
+}
